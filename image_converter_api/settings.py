f64887ba9d902dc2a271c058888bbd2e3c7fb1ed
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2025-01-22 10:12:53 -0500

    update(settings.py): db use changed from Sqlite to PostgreSQL

--- image_converter_api/settings.py
+++ image_converter_api/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'image_converter_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('PGDATABASE'),
+        'USER': env('PGUSER'),
+        'PASSWORD': env('PGPASSWORD'),
+        'HOST': env('PGHOST'),
+        'PORT': env('PGPORT'),
     }
 }
 
